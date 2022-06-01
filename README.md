dbe920d4ab3a740324c1560ef71bcf3a5678eb64
João Santos <joaosantosdev@hotmail.com>
2022-05-31 21:26:27 -0300

    melhorando docker

---
+++ README.md
@@ -0,0 +1,19 @@
+# TCC - Backend
+
+### Framework
+* Java + Spring
+
+
+#### Outras partes que compõem a aplicação
+
+- [Frontend](https://github.com/joaosantosdev/feirouu-frontend)
+
+#### Atenção
+* Baixar o jdk, maven e docker
+
+### Rodar a aplicação com Docker
+```
+docker-compose up -d
+Rodando em: http://localhost:8082/
+```
+
--- scripts/data.sql
+++ scripts/data.sql
@@ -1,4 +1,49 @@
+ create table if not exists estado (
+       id  serial not null,
+        nome varchar(255),
+        uf varchar(255),
+        primary key (id)
+);
+
+ create table if not exists cidade (
+       id  serial not null,
+        nome varchar(255),
+        estado_id int,
+        primary key (id)
+    );
+
+
 DELETE FROM cidade WHERE id >= 1;
+DELETE FROM estado WHERE id >= 1;
+
+INSERT INTO estado VALUES (1, 'Acre', 'AC');
+INSERT INTO estado VALUES (2, 'Alagoas', 'AL');
+INSERT INTO estado VALUES (3, 'Amazonas', 'AM');
+INSERT INTO estado VALUES (4, 'Amapá', 'AP');
+INSERT INTO estado VALUES (5, 'Bahia', 'BA');
+INSERT INTO estado VALUES (6, 'Ceará', 'CE');
+INSERT INTO estado VALUES (7, 'Distrito Federal', 'DF');
+INSERT INTO estado VALUES (8, 'Espirito Santo', 'ES');
+INSERT INTO estado VALUES (9, 'Goiás', 'GO');
+INSERT INTO estado VALUES (10, 'Maranhão', 'MA');
+INSERT INTO estado VALUES (11, 'Minas Gerais', 'MG');
+INSERT INTO estado VALUES (12, 'Mato Grosso do Sul', 'MS');
+INSERT INTO estado VALUES (13, 'Mato Grosso', 'MT');
+INSERT INTO estado VALUES (14, 'Pará', 'PA');
+INSERT INTO estado VALUES (15, 'Paraíba', 'PB');
+INSERT INTO estado VALUES (16, 'Pernambuco', 'PE');
+INSERT INTO estado VALUES (17, 'Piauí', 'PI');
+INSERT INTO estado VALUES (18, 'Paraná', 'PR');
+INSERT INTO estado VALUES (19, 'Rio de Janeiro', 'RJ');
+INSERT INTO estado VALUES (20, 'Rio Grande do Norte', 'RN');
+INSERT INTO estado VALUES (21, 'Rondônia', 'RO');
+INSERT INTO estado VALUES (22, 'Roraima', 'RR');
+INSERT INTO estado VALUES (23, 'Rio Grande do Sul', 'RS');
+INSERT INTO estado VALUES (24, 'Santa Catarina', 'SC');
+INSERT INTO estado VALUES (25, 'Sergipe', 'SE');
+INSERT INTO estado VALUES (26, 'São Paulo', 'SP');
+INSERT INTO estado VALUES (27, 'Tocantins', 'TO');
+
 
 /* ******************************** A c r e ********************************* */
 
